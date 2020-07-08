7ecdde6b6acb59bcc0271163f103553674a44474
fredcfreitas <fredcfreitas@gmail.com>
2020-07-08 16:18:26 -0300

    Final remarks in phi-values vanilla script. Some corrections after Ronaldo's meeting.

--- phi_values.py
+++ phi_values.py
@@ -1,23 +1,50 @@
 #!/usr/bin/env python3
-# USAGE: python3 phi_values.py  #PDB_BASE #XTC_FILE #FILE.count_FILE #OUTPUT
-# *boundaries* should be in the order: unfolded, transition state; folded
-# in all cases.
+# USAGE: python3 phi_values.py #MODEL(AA or CA) #PDB_BASE #XTC_FILE \
+# #file.cont_FILE #OUTPUT *boundaries* [should be in the order: unfolded \
+# (lower and upper limit), transition state (lower and upper limit); folded \
+# (lower and upper limit) in all cases.
 
 import sys
 import itertools
 import numpy as np
 import mdtraj as md
 
+def evaluate_r_initial(contacts, model="AA"):
+    """
+    Function to evaluate initial pairwise distances accordingly the model \
+    simulated.
+    Input:
+     contacts_list - Array with the definitions from pairs section of the \
+     forcefield (TPR file.)
+     model - AA = All-Atom; CA = Carbon_alpha Coarse-Grained
+    Output:
+     r_initial - vector with the initial distance of each pair.
+    """
+    if model == "CA":
+        r_initial = np.power(np.divide(np.multiply(contacts[:, 4], 1.2), \
+                                                   contacts[:, 3]), \
+                             np.divide(1, 2))
+    elif model == "AA":
+        r_initial = np.power(np.divide(np.multiply(contacts[:, 4], 2), \
+                                                   contacts[:, 3]),\
+                             np.divide(1, 6))
+    else:
+        print("You have not provided an unimplemented model.")
+        exit()
+    return r_initial
+
 
 def return_ordered(num_1, num_2):
     """Function to get two numbers and return the pair in ascending order"""
     if num_1 == num_2:
         print("Both are the same. \n Please restart with the rigth values.")
-        return num_1, num_2
+        pair = num_1, num_2
     elif num_1 < num_2:
-        return num_1, num_2
+        pair = num_1, num_2
     else:
-        return num_2, num_1
+        pair = num_2, num_1
+    return pair
+
 
 def test_overlap_pairs(pair_1, pair_2):
     """
@@ -56,7 +83,7 @@ def phi_i(pdb_file, xtc_file, pairs_indexes, r_initial, boundaries, \
     """
     Function to evaluate the phi-value for each atom/residue.
     Input:
-     pdb_file - File with your structure (PDB or GRO files for instance).
+     pdb_file - File with your structure (PDB or GRO files, for instance).
      xtc_file - Trajectory.
      pairs_indexes - Numpy array Nx2 with the pairs to be used to evaluate \
      the contacts. (The first two columns of the pairs section in the TPR file \
@@ -65,13 +92,16 @@ def phi_i(pdb_file, xtc_file, pairs_indexes, r_initial, boundaries, \
      threshold - Value to be used as a threshold to evaluate the contacts.
      chunk - Size of each chunk in which the trajectory will be analyzed.
     Output
+     phi - Numpy array with phi_values as a function of i - (atom/residue)
     """
 
     cutoff = np.multiply(threshold, r_initial)
 
     results = np.zeros((np.shape(boundaries)[0], np.shape(r_initial)[0]))
 
-    atoms_involved = np.unique(pairs_indexes)
+    # Correcting the numbering of atoms/residues involved.
+    atoms_indexes = np.unique(pairs_indexes)
+    atoms_involved = np.add(atoms_indexes, 1)
 
     # The last number of frames will store the total number for sanity check.
     n_frames = np.zeros(np.shape(boundaries)[0] + 1)
@@ -83,7 +113,8 @@ def phi_i(pdb_file, xtc_file, pairs_indexes, r_initial, boundaries, \
         below_threshold = np.less_equal(trajectory, cutoff)
         # Generate a matrix with 1 where contacts are formed.
         num_below_threshold = np.multiply(below_threshold, 1)
-        contacts_time = np.sum(num_below_threshold, axis=1) # (number of contact per timestep)
+        # (number of contact per timestep)
+        contacts_time = np.sum(num_below_threshold, axis=1)
         for i, pair in enumerate(boundaries):
             idx = np.logical_and(np.greater_equal(contacts_time, pair[0]), \
                                  np.less_equal(contacts_time, pair[1]))
@@ -105,12 +136,12 @@ def phi_i(pdb_file, xtc_file, pairs_indexes, r_initial, boundaries, \
 
     # The probability for each atom is given multiplying the probability of all\
     # pairs with this atom.
-    for i, atom in enumerate(atoms_involved):
+    for i, atom in enumerate(atoms_indexes):
         idx_atom = np.isin(pairs_indexes, atom).any(axis=1)
         pij_transition_unfolded[i] += \
                     np.sum(np.subtract(results[1, idx_atom], \
                                        results[0, idx_atom]))
-        pij_folded_unfolded += \
+        pij_folded_unfolded[i] += \
                     np.sum(np.subtract(results[2, idx_atom], \
                                        results[0, idx_atom]))
 
@@ -118,15 +149,17 @@ def phi_i(pdb_file, xtc_file, pairs_indexes, r_initial, boundaries, \
     assert np.less_equal(np.sum(n_frames[:-1]), n_frames[-1])
 
     phi = np.nan_to_num(np.divide(pij_transition_unfolded, pij_folded_unfolded))
-    atoms_indexes = np.add(np.arange(np.shape(atoms_involved)[0]), 1)
 
-    # # This return will give phi(i) where i is the atom/residue number given.
-    # return np.concatenate((fromlistto1d(atoms_involved), \
+    # # This return will give phi(i) where i is the atom/residue number given \
+    # # by the forcefield.
+    return np.concatenate((fromlistto1d(atoms_involved), \
+                           fromlistto1d(phi)), axis=1)
+
+    # atoms_indexes = np.add(np.arange(np.shape(atoms_involved)[0]), 1)
+    # # This return will give phi(i) where i is the i-th atom/residue.
+    # return np.concatenate((fromlistto1d(atoms_indexes), \
     #                        fromlistto1d(phi)), axis=1)
 
-    # # This return will give phi(i) where i is the i-th atom/residue.
-    return np.concatenate((fromlistto1d(atoms_indexes), \
-                           fromlistto1d(phi)), axis=1)
 
 
 def main():
@@ -135,7 +168,7 @@ def main():
     #
     # xtc_file = sys.argv[2]
     #
-    pairs_contacts = sys.argv[3]
+    pairs_contacts = sys.argv[4]
     #
     # output_file = sys.argv[4]
 
@@ -144,20 +177,19 @@ def main():
     # This MUST be done due to python indexes, that starts from zero
     pairs_indexes = np.subtract(contacts[:, 0:2], 1)
 
-    r_initial = np.power(np.divide(np.multiply(contacts[:, 4], 2), contacts[:, \
-                                               3]), np.divide(1, 6))
+    r_initial = evaluate_r_initial(contacts, model=str(sys.argv[1]))
 
     # Defining the transition state
-    first_lower, first_upper = return_ordered(float(sys.argv[5]), \
-                                              float(sys.argv[6]))
+    first_lower, first_upper = return_ordered(float(sys.argv[6]), \
+                                              float(sys.argv[7]))
 
     # Defining the unfolded state
-    second_lower, second_upper = return_ordered(float(sys.argv[7]), \
-                                                 float(sys.argv[8]))
+    second_lower, second_upper = return_ordered(float(sys.argv[8]), \
+                                                 float(sys.argv[9]))
 
     # Defining the folded state
-    third_lower, third_upper = return_ordered(float(sys.argv[9]), \
-                                              float(sys.argv[10]))
+    third_lower, third_upper = return_ordered(float(sys.argv[10]), \
+                                              float(sys.argv[11]))
 
     # Building the boundaries array
     boundaries = np.asarray([[first_lower, first_upper], [second_lower, \
@@ -167,11 +199,11 @@ def main():
     assert checking_intervals_overlaping(boundaries)
 
 
-    final_phi_i = phi_i(sys.argv[1], sys.argv[2], pairs_indexes, r_initial, \
+    final_phi_i = phi_i(sys.argv[2], sys.argv[3], pairs_indexes, r_initial, \
                         boundaries)
 
-    np.savetxt(sys.argv[4], final_phi_i)
-    return
+    np.savetxt(sys.argv[5], final_phi_i)
+    return 0
 
 if __name__ == "__main__":
     main()
