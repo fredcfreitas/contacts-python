b15facacc481af723f760486acbd876cffcd4331
frederico <fredcfreitas@gmail.com>
2026-08-13 16:27:05 -0300

    Adding test to evaluating r_initial from pdb

--- share/test_contact_probability.py
+++ share/test_contact_probability.py
@@ -8,9 +8,32 @@ import contact_probability as cp
 import contacts_chunk as cc
 
 
+def test_evaluate_r_from_pdb():
+    """
+    Function to test the evaluation of initial pairwise distances \
+    accordingly the simulated model.
+    Input:
+     contacts_list - Array with the definitions from pairs section of the \
+     forcefield (TPR file.)
+     model - AA = All-Atom; CA = Carbon_alpha Coarse-Grained
+    Output:
+     r_initial - vector with the initial distance of each pair.
+    """
+    threshold_distances = 0.001
+    pairs_section_filepath = 'share/ci2-AA-contacts.dat'
+    pairs_section = np.genfromtxt(pairs_section_filepath)
+    pairs_indexes = np.subtract(pairs_section[:, 0:2], 1)
+    pdb_file = 'share/ci2-adjusted.pdb'
+    pdb_loaded = md.load(pdb_file)
+    initial_distances = md.compute_distances(pdb_loaded, pairs_indexes)
+    r_initial = cc.evaluate_r_from_pdb(pairs_section, pdb_file)
+    test = np.less_equal(np.absolute(np.subtract(r_initial, \
+        initial_distances)), threshold_distances).all()
+    assert test
+
 def test_evaluate_r_initial():#pairs_section, initial_distances):
     """
-    Function to test the evaluation of  initial pairwise distances \
+    Function to test the evaluation of initial pairwise distances \
     accordingly the simulated model.
     Input:
      contacts_list - Array with the definitions from pairs section of the \
